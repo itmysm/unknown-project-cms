ec84e48ba665bf210e470da4844d7538165d3182
itmysm <dev.mysm@gmail.com>
2023-10-19 21:59:11 +0330

    feat: initial button types and make a btn component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Colors } from "./_components/colors/colors";
+import { Button } from "./_components/button/buttton";
 
 export default function Home() {
   return (
@@ -36,6 +37,12 @@ export default function Home() {
           />
         </div>
       </section>
+
+      <div className="container">
+        <Button variant="info" size="large" shape="wide" animatedIcon={true}>
+          ثبت نام
+        </Button>
+      </div>
     </>
   );
 }
