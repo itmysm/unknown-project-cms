4d16d6543674309b7948016a4e088213e76e1441
Meysam Kiani <dev.mysm@gmail.com>
2023-10-24 10:39:31 +0300

    redesign storybook theme and add font

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,13 +1,33 @@
-import type { Preview } from "@storybook/react";
+// Replace your-framework with the framework you are using (e.g., react, vue3)
+import { Preview } from '@storybook/react';
+
+import { Title, Subtitle, Description, Primary, Controls, Stories } from '@storybook/blocks';
+import React from 'react';
+import '../src/app/globals.css';
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    doc: {
+      controls: { exclude: ['controls'] },
+    },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/i,
+        date: /Date$/,
       },
+    },
+    docs: {
+      showPanel: false,
+      // page: () => (
+      //   <>
+      //     <Title />
+      //     <Subtitle />
+      //     <Description />
+      //     <Controls  />
+      //     <Stories />
+      //   </>
+      // ),
     },
   },
 };
