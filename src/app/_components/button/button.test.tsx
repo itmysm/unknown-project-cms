298f27212716537e67bd90e82fe47330d93a7366
itmysm <dev.mysm@gmail.com>
2023-12-30 10:49:36 +0330

    refactor: simple tests for buttons

--- src/app/_components/button/button.test.tsx
+++ src/app/_components/button/button.test.tsx
@@ -1,7 +1,26 @@
 import { describe } from "node:test";
+import { Button } from ".";
+import { render, screen } from "@testing-library/react";
 
-describe("Button", () => {
-  it("testing button", () => {
-    
+describe("Button Component", () => {
+  test("renders a default button", () => {
+    const div = document.createElement("div");
+    // ReactDOM.render(, div);
+    // document.body.appendChild(div);
+    const { getByText } = render(<Button>Click here</Button>);
+    expect(getByText("Click here")).toBeInTheDocument();
+  });
+
+  test("disables the button when isDisabled prop is true", () => {
+    render(<Button isDisabled={true}>Click here</Button>);
+    expect(screen.getByRole("button")).toBeDisabled();
+  });
+
+  test("applies the correct the css class for different button varients", () => {
+    const { rerender } = render(<Button variant="primary">Click here</Button>);
+    expect(screen.getByRole("button")).toHaveClass("btn-primary");
+
+    rerender(<Button variant="info">Click here</Button>);
+    expect(screen.getByRole("button")).toHaveClass("btn-info");
   });
 });
