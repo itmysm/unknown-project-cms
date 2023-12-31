6a24c69d115267e93b4842067de2a1cc7d303dbc
itmysm <dev.mysm@gmail.com>
2023-12-31 10:48:59 +0330

    feat: fetch data from server and get list courses from api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
+"client";
+
 import Image from "next/image";
 import { Button } from "./_components/button/button";
 import { HomeHeroSection } from "./_components/home-hero-section/home-hero-section";
+import { CourseSummary } from "@/types/course-summary.interface";
 
-export default function Home() {
+async function getNewestCourses(count: number): Promise<CourseSummary[]> {
+  const res = await fetch(
+    `https://api.classbon.com/api/courses/newest/${count}`,
+    {
+      next: {
+        revalidate: 24 * 60 * 60,
+      },
+    }
+  );
+  return res.json();
+}
+
+export default async function Home() {
+  const newestCourses = await getNewestCourses(4);
   return (
     <>
       <HomeHeroSection />
