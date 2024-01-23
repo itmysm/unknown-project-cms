09cb0d5d600c44c5e92ee496ec48f680921f066b
itmysm <dev.mysm@gmail.com>
2024-01-23 13:21:31 +0330

    refactor: worked on testimonial component

---
+++ src/app/_components/testimonial/testimonial.type.ts
@@ -0,0 +1,8 @@
+import { Testimonial } from "@/types/testimonials.type";
+export type TestimonialProps = {
+    testimonial: Testimonial
+}
+
+export type TestimonialListProps = {
+    testimonials: Testimonial[]
+}
